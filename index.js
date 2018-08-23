9368424980dc0f22e9162c01c00916643ce8042d
amitvagner@gmail.com <amitvagner@gmail.com>
2018-08-23 13:30:23 +0300

    fix print format and trace index

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ const addLocation = (args) => {
         return args
     }
     return args.map(val => {
-
         if (typeof val === 'string') {
             return trace[traceIndex].getFileName() + ":" + trace[traceIndex].getLineNumber() + "\n" + val;
         }
-        else if (typeof val === 'object' && Array.isArray(val) === false && val !== null && val instanceof Error === false  ) {
-            const clone = JSON.parse(JSON.stringify(val));
-            clone.consoleTrace = trace[traceIndex].getFileName() + ":" + trace[traceIndex].getLineNumber();
-            return val;
+        else if (typeof val === 'object' && Array.isArray(val) === false && val !== null  ) {
+            if(val instanceof Error === false ){
+
+                const clone = JSON.parse(JSON.stringify(val));
+                clone.consoleFrom = trace[traceIndex].getFileName() + ":" + trace[traceIndex].getLineNumber();
+                return clone;
+            }
+            else{
+                val.consoleFrom =  trace[traceIndex].getFileName() + ":" + trace[traceIndex].getLineNumber();
+            }
+
         }
         return val;
     });
