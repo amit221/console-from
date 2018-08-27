042bb29be251c914fd3f776e20d6159cfa3325cf
amitvagner@gmail.com <amitvagner@gmail.com>
2018-08-27 11:06:34 +0300

    fix bug when file name is null

--- index.js
+++ index.js
@@ -5,23 +5,26 @@ const path = require('path');
 const dirname = path.resolve(__dirname);
 const deletePath = dirname.substr(0, dirname.indexOf('node_modules'));
 
-const addLocation = (args) => {
+const addLocation = () => {
     const trace = stackTrace.get();
     if (trace.length < 3) {
-        return args;
+        return;
     }
-
-    const currentFile = trace[traceIndex].getFileName().replace(deletePath, '');
+    const fileName = trace[traceIndex].getFileName();
+    if (fileName === null) {
+        return;
+    }
+    const currentFile = fileName.replace(deletePath, '');
     tempConsoleLog("\x1b[36m%s\x1b[0m", currentFile + ":" + trace[traceIndex].getLineNumber());
 
-    return args;
+
 };
 
 
 const tempConsoleError = console.error;
 
 global.console.error = function (...args) {
-    args = addLocation(args);
+    addLocation(args);
     tempConsoleError.apply(console, args);
 
 };
